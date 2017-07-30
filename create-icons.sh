3176dcb07204feb80cadcba6eef23d32654f6f71
tdillon <tdillon@users.noreply.github.com>
2017-07-30 15:53:50 -0400

    Manifest updates and icon generation.

---
+++ create-icons.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env sh
+rsvg -w 512 -h 512 src/icon.svg src/assets/icon-512x512.png
+rsvg -w 256 -h 256 src/icon.svg src/assets/icon-256x256.png
+rsvg -w 192 -h 192 src/icon.svg src/assets/icon-192x192.png
+rsvg -w 168 -h 168 src/icon.svg src/assets/icon-168x168.png
+rsvg -w 152 -h 152 src/icon.svg src/assets/icon-152x152.png
+rsvg -w 144 -h 144 src/icon.svg src/assets/icon-144x144.png
+rsvg -w 128 -h 128 src/icon.svg src/assets/icon-128x128.png
+rsvg -w  96 -h 96 src/icon.svg src/assets/icon-96x96.png
+rsvg -w  72 -h 72 src/icon.svg src/assets/icon-72x72.png
+rsvg -w  48 -h 48 src/icon.svg src/assets/icon-48x48.png
+rsvg -w  16 -h 16 src/icon.svg src/assets/icon-16x16.png
