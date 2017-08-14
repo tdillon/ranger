42436a0587a18546b06cbb1164b4fe4afe5eb2ce
tdillon <tdillon@users.noreply.github.com>
2017-08-14 13:03:50 -0400

    Update build info.

--- src/app/buildInfo.ts
+++ src/app/buildInfo.ts
@@ -5,6 +5,6 @@ export const buildInfo = {
     version: '0.0.0',
     buildNumber: 1,
     buildUrl: 'https://travis-ci.org/tdillon/ranger/builds/232250457',
-    commit: 'cf988d2',
+    commit: 'cf988d2f8d4c6d82fe80ecc4c2352a3b9abb1aa9',
     repoUrl: 'https://github.com/tdillon/ranger/commit/cf988d2f8d4c6d82fe80ecc4c2352a3b9abb1aa9'
 };
