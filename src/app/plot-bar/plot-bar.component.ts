e6c3901b4eb2b3701078cdcee01aa55cc3863528
tdillon <tdillon@users.noreply.github.com>
2017-08-22 18:30:40 -0400

    Use utilities for getLocation.

--- src/app/plot-bar/plot-bar.component.ts
+++ src/app/plot-bar/plot-bar.component.ts
@@ -3,6 +3,7 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { LatLong } from '../lat-long';
 import { DataService } from '../data.service';
 import { LocationService } from '../location.service';
+import { Utilities } from '../utilities';
 
 @Component({
   selector: 'app-plot-bar',
@@ -46,7 +47,7 @@ export class PlotBarComponent implements AfterViewInit {
 
     this.dataService.getTargets().subscribe(t => {
       this.targets = t.map(target => {
-        return { ...target, distance: this.getDistance(target, this.dataService.currentBase) };
+        return { ...target, distance: Utilities.getDistance(target, this.dataService.currentBase) };
       });
 
       this.draw();
@@ -85,7 +86,7 @@ export class PlotBarComponent implements AfterViewInit {
 
     if (this.locationService.currentLocation) {
       currentLocation = this.locationService.currentLocation;
-      currentDistance = this.getDistance(new LatLong(currentLocation.coords), this.dataService.currentBase);
+      currentDistance = Utilities.getDistance(new LatLong(currentLocation.coords), this.dataService.currentBase);
     }
 
     if (currentDistance > max) {
@@ -129,23 +130,7 @@ export class PlotBarComponent implements AfterViewInit {
     }
 
     // Current distance
-    this.drawMarker(iconSize +  5 * dpr, '#09c', p + currentDistance * wr, c - lineWidth);
-  }
-
-  private getDistance(l1: LatLong, l2: LatLong) {
-    // http:// en.wikipedia.org/wiki/Geographical_distance#Tunnel_distance
-    // radians are needed for calculations
-    const lat1 = l1.latitude * Math.PI / 180,
-      lon1 = l1.longitude * Math.PI / 180,
-      lat2 = l2.latitude * Math.PI / 180,
-      lon2 = l2.longitude * Math.PI / 180,
-      R = 6371.009,  // earth's radius in KM
-      X = Math.cos(lat2) * Math.cos(lon2) - Math.cos(lat1) * Math.cos(lon1),
-      Y = Math.cos(lat2) * Math.sin(lon2) - Math.cos(lat1) * Math.sin(lon1),
-      Z = Math.sin(lat2) - Math.sin(lat1),
-      C = Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2) + Math.pow(Z, 2)),
-      D = R /*KM*/ * C * 1000 /*M/KM*/ / .9144 /*M/Y*/;
-    return Math.round(D);  // YARDS
+    this.drawMarker(iconSize + 5 * dpr, '#09c', p + currentDistance * wr, c - lineWidth);
   }
 
   private drawMarker(height, color, x, y) {
