4e1acb064b4961710da84bef477f4a9c595d95e7
tdillon <tdillon@users.noreply.github.com>
2018-05-06 15:38:36 -0400

    Upgrade to Angular 6.

--- create-icons.sh
+++ create-icons.sh
@@ -1,11 +1,9 @@
 #/usr/bin/env sh
 rsvg-convert -w 512 -h 512 -o src/assets/icon-512x512.png src/icon.svg
-rsvg-convert -w 256 -h 256 -o src/assets/icon-256x256.png src/icon.svg
+rsvg-convert -w 384 -h 384 -o src/assets/icon-384x384.png src/icon.svg
 rsvg-convert -w 192 -h 192 -o src/assets/icon-192x192.png src/icon.svg
-rsvg-convert -w 168 -h 168 -o src/assets/icon-168x168.png src/icon.svg
 rsvg-convert -w 152 -h 152 -o src/assets/icon-152x152.png src/icon.svg
 rsvg-convert -w 144 -h 144 -o src/assets/icon-144x144.png src/icon.svg
 rsvg-convert -w 128 -h 128 -o src/assets/icon-128x128.png src/icon.svg
 rsvg-convert -w  96 -h 96 -o src/assets/icon-96x96.png src/icon.svg
 rsvg-convert -w  72 -h 72 -o src/assets/icon-72x72.png src/icon.svg
-rsvg-convert -w  48 -h 48 -o src/assets/icon-48x48.png src/icon.svg
